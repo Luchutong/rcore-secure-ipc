0760a2517b4711e98d128ab6c1410df061267fae
Luchutong <184732567+Luchutong@users.noreply.github.com>
2026-08-31 21:09:45 +0800

    fix: satisfy user access lifetime bounds

--- os/src/mm/user_access.rs
+++ os/src/mm/user_access.rs
@@ -7,12 +7,12 @@ use super::{translated_ref, translated_refmut};
 use crate::security::IpcResult;
 
 /// Copy a plain value from user memory.
-pub fn copy_from_user<T: Copy>(token: usize, src: *const T) -> IpcResult<T> {
+pub fn copy_from_user<T: Copy + 'static>(token: usize, src: *const T) -> IpcResult<T> {
     Ok(*translated_ref(token, src))
 }
 
 /// Copy a plain value to user memory.
-pub fn copy_to_user<T: Copy>(token: usize, dst: *mut T, value: &T) -> IpcResult<()> {
+pub fn copy_to_user<T: Copy + 'static>(token: usize, dst: *mut T, value: &T) -> IpcResult<()> {
     *translated_refmut(token, dst) = *value;
     Ok(())
 }
